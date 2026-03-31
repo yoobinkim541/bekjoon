b02c80946e20ab651d5f86c939f935c39a1923db
Yoobin Kim <yoobinkim06@gmail.com>
2026-03-31 19:44:44 +0900

    status: modified

--- c_study/c++_study/17626.cpp
+++ c_study/c++_study/17626.cpp
@@ -9,7 +9,7 @@ int main(void){
     vector<int> dp(N+1);
     dp[1] = 1;
     for(int i = 2; i <= N; i++){
-        dp[i] = i; // Initialize with the maximum possible count (all 1s)
+        dp[i] = i;
         for(int j = sqrt(i); j > 0; j--){
             dp[i] = min(dp[i], dp[i-j*j]+1);
         }
